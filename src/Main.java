2a17b5a5d2d4e3e8eb9c174941d52019795a38a3
leila <leila.m2006131@gmail.com>
2025-03-17 23:06:36 +0330

    add Main method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            String word = "";
+
+            if (i % 3 == 0) {
+                word += "Fizz";
+            }
+            if (i % 5 == 0) {
+                word += "Buzz";
+            }
+
+            if (word == "") {
+                word = String.valueOf(i);
+            }
+
+            System.out.println(word);
+        }
     }
 }
